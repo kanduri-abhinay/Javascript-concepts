e7ceb64a4297df2f8b07cbcd86501fe96af0439c
Kanduri Abhinay <kanduriabhinay@gmail.com>
2024-11-19 09:17:06 +0530

    currying update

--- currying.js
+++ currying.js
@@ -14,3 +14,22 @@ multiply = function (x) {
 
 const multiplyByThree = multiply(3);
 console.log(multiplyByThree(5));
+
+const join = (a, b, c) => {
+  return `${a}_${b}_${c}`;
+};
+function curry(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) return fn.apply(null, args);
+    else {
+      return function (...args2) {
+        return curried.apply(null, [...args, ...args2]);
+      };
+    }
+  };
+}
+const curriedJoin = curry(join);
+const _ = curry.placeholder;
+console.log(curriedJoin(1, 2, 3)); // '1_2_3'
+console.log(curriedJoin(1, 2)(3)); // '1_2_3'
+console.log(curriedJoin(1)(2, 3)); // '1_2_3'
